acccecde3ebed4f9740164ad1ec28b8abc4bb6e4
son <son@lincolnix.net>
2014-08-19 11:36:16 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EricBrown::Application.config.secret_key_base = '405627adac70dba7d29f077906d670d4cd1fc5ad59b48d2353706efc0326ce5a531b1ea3b0d0b4b4bf8f17c04f0599aa05e40ae9cd6a3c3a4aea58936768bb61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EricBrown::Application.config.session_store :cookie_store, key: '_EricBrown_session'
