9c444b24f2697a4d244f68ff6df652532124e828
son <son@lincolnix.net>
2014-08-25 18:27:08 +0000

    style

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,10 @@ EricBrown::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  unless Rails.env.production?
+    ENV['COMFY_USERNAME'] = 'username'
+    ENV['COMFY_PASSWORD'] = 'password'
+  end
+
 end
